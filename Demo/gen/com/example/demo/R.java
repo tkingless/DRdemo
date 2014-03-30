93e0a18dcb93eb9dc6d7d336f6f438ed2f3755bc
tkingless <unstashworld@gmail.com>
2014-03-30 17:31:59 +0800

    2mergeBefore

--- Demo/gen/com/example/demo/R.java
+++ Demo/gen/com/example/demo/R.java
@@ -28,8 +28,9 @@ public final class R {
         public static final int pic1_bg=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
+        public static final int action_settings=0x7f080013;
         public static final int btn_p1_company=0x7f080003;
+        public static final int button1=0x7f080012;
         public static final int buttonEnter=0x7f080005;
         public static final int button_p1_return=0x7f080002;
         public static final int chatReturnPrev=0x7f08000a;
@@ -39,6 +40,7 @@ public final class R {
         public static final int chat_dialog_scroll=0x7f08000f;
         public static final int chat_dialog_text=0x7f080010;
         public static final int chat_dialog_time=0x7f08000e;
+        public static final int chat_keyboardSwitch=0x7f080011;
         public static final int chat_person=0x7f08000c;
         public static final int chat_title=0x7f080004;
         public static final int chat_weChatLogo=0x7f08000b;
@@ -58,17 +60,18 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int Enter=0x7f05000b;
+        public static final int Enter=0x7f05000c;
         public static final int Person=0x7f050002;
         public static final int WeChat=0x7f050001;
-        public static final int action_settings=0x7f050008;
-        public static final int app_name=0x7f050006;
+        public static final int action_settings=0x7f050009;
+        public static final int app_name=0x7f050007;
         public static final int back=0x7f050000;
-        public static final int companyExclusive=0x7f05000a;
+        public static final int companyExclusive=0x7f05000b;
+        public static final int companyService=0x7f050006;
         public static final int dialog_content=0x7f050005;
         public static final int dialog_time=0x7f050004;
-        public static final int generalUser=0x7f050009;
-        public static final int hello_world=0x7f050007;
+        public static final int generalUser=0x7f05000a;
+        public static final int hello_world=0x7f050008;
         public static final int mockCompany=0x7f050003;
     }
     public static final class style {
